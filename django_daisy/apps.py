6ee704f5bdb3a40cd56914dc82f42bbb3fee59d5
yaghoubi <stick2me3@gmail.com>
2024-10-25 00:31:17 +0330

    enable custom dashboard site class

--- django_daisy/apps.py
+++ django_daisy/apps.py
@@ -1,9 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin import sites
+from django.utils.module_loading import import_string
 
 from . import module_settings  # noqa, load module settings
-from .admin import DaisyAdminSite
+
+if default_admin_site := getattr(settings, 'DEFAULT_ADMIN_SITE_CLASS'):
+    AdminSiteClass = import_string(default_admin_site)
+else:
+    AdminSiteClass = import_string('django_daisy.admin.DaisyAdminSite')
 
 
 class LogEntryAdmin(admin.ModelAdmin):
@@ -16,7 +22,7 @@ class DefaultAppConfig(AppConfig):
     default = True
 
     def ready(self):
-        site = DaisyAdminSite()
+        site = AdminSiteClass()
         from .logentry_admin import LogentryAdmin  # noqate
         from django.contrib.admin.models import LogEntry  # noqate
 
