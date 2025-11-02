6a7c84e556064fca756273d3185c712bbf45b264
hypy <hossein.yaghoubi13@gmail.com>
2025-11-02 12:14:22 +0330

    fix issue #63

--- django_daisy/templatetags/dash_tags.py
+++ django_daisy/templatetags/dash_tags.py
@@ -63,18 +63,17 @@ def daisy_result_list(cl):
     """
     Display the headers and data list together.
     """
-    from django.conf import settings
     headers = list(result_headers(cl))
     num_sorted_fields = 0
     for h in headers:
         if h["sortable"] and h["sorted"]:
             num_sorted_fields += 1
+
     return {
         "cl": cl,
         "result_hidden_fields": list(result_hidden_fields(cl)),
         "result_headers": headers,
         "num_sorted_fields": num_sorted_fields,
-        'extra_table_classes': settings.DAISY_SETTINGS.get('CHANGELIST_TABLE_STYLE'),
         "results": list(results(cl)),
     }
 
